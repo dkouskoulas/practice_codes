a26e5048f5e878f85480b0fc06b318ce25daa631
dkouskoulas <76634661+dkouskoulas@users.noreply.github.com>
2026-09-12 22:11:51 -0400

    Fix probability union/intersection logic

--- class_prob_calc.py
+++ class_prob_calc.py
@@ -1,6 +1,6 @@
 
 class Probability:
-    def __init__(self, p_A, p_B = None, p_A_and_B = None, p_A_or_B = None, independent = False):
+    def __init__(self, p_A, p_B=None, p_A_and_B=None, p_A_or_B=None, independent=False):
         self.p_A = p_A
         self.p_B = p_B
         self.p_A_and_B = p_A_and_B
@@ -13,18 +13,8 @@ class Probability:
     def calc_complement_p_B(self):
         return 1 - self.p_B
 
-    def calc_intersection(self, p_B = None, p_A_and_B = None, p_A_or_B = None, independent = None, _use_union_fallback = False):
-
-        if p_B is not None:
-            self.p_B = p_B
-        if p_A_and_B is not None:
-            self.p_A_and_B = p_A_and_B
-        if p_A_or_B is not None:
-            self.p_A_or_B = p_A_or_B
-        if independent is None:
-            independent = self.independent
-
-        if independent:
+    def calc_intersection(self):
+        if self.independent:
             if self.p_B is None:
                 raise ValueError("p_B is required when events are independent.")
             return self.p_A * self.p_B
@@ -35,38 +25,26 @@ class Probability:
         if self.p_A_or_B is not None and self.p_B is not None:
             return self.p_A + self.p_B - self.p_A_or_B
 
-        if self.p_B is not None and not _use_union_fallback:
-            union_value = self.calc_union(p_B=self.p_B, independent=False, _use_intersection_fallback=True)
-            return self.p_A + self.p_B - union_value
+        if self.p_B is not None:
+            return self.p_A + self.p_B - self.calc_union()
 
         raise ValueError("Insufficient information to compute the intersection.")
 
-    def calc_union(self, p_B = None, p_A_and_B = None, p_A_or_B = None, independent = None, _use_intersection_fallback = False):
-
-        if p_B is not None:
-            self.p_B = p_B
-        if p_A_and_B is not None:
-            self.p_A_and_B = p_A_and_B
-        if p_A_or_B is not None:
-            self.p_A_or_B = p_A_or_B
-        if independent is None:
-            independent = self.independent
-
+    def calc_union(self):
         if self.p_A_or_B is not None:
             return self.p_A_or_B
 
         if self.p_B is None:
             raise ValueError("p_B is required to compute the union.")
 
-        if independent:
+        if self.independent:
             return self.p_A + self.p_B - (self.p_A * self.p_B)
 
         if self.p_A_and_B is not None:
             return self.p_A + self.p_B - self.p_A_and_B
 
-        if not _use_intersection_fallback:
-            intersection_value = self.calc_intersection(p_B=self.p_B, independent=False, _use_union_fallback=True)
-            return self.p_A + self.p_B - intersection_value
+        if self.p_B is not None:
+            return self.p_A + self.p_B - self.calc_intersection()
 
         raise ValueError("Insufficient information to compute the union.")
 
